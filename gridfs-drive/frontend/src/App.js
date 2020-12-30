35c9303c3111601f8e1303c38ac018c65cd52a46
Anil Raj <anilraje122@gmail.com>
2020-12-30 22:02:11 +0530

    Add redux store

--- gridfs-drive/frontend/src/App.js
+++ gridfs-drive/frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import routes from "./routing/routes";
+import { useRoutes } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const routing = useRoutes(routes);
+  return <SnackbarProvider>{routing}</SnackbarProvider>;
+};
 
 export default App;
